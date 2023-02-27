4646b3906efe021973dba3fdf9b0f7628702d19d
Ioana Carmen Pasca <ic.pasca.2021@alumnos.urjc.es>
2023-02-27 21:18:49 +0100

    works
    Signed-off-by: Ioana Carmen Pasca <ic.pasca.2021@alumnos.urjc.es>

--- practise-1/filebysize.sh
+++ practise-1/filebysize.sh
@@ -4,29 +4,33 @@
 #    File: filebysize.sh
 #    Author: Ioana Carmen Pasca
 #    Date: 25/02/2023
-#    Goal: 
-#          then the program shows their their addition, subtraction,
-#          division and multiplication
+#    Goal: Prints the file with the biggest size 
 # -------------------------------------------------------------------
 
 # Constants
 arg1="$1"
+arg2="$2"
+dir=$(pwd)
 
 # Variables
 files=$(ls -l | awk '{ size=$5 ; name=$9; printf(size/1024 " " name "\n") }')
 
 usage() {  # Error of usage
-	echo "usage: .filebysize [OPNION]..." 1>&2
+	echo "usage: ./filebysize [OPTION]..." 1>&2
 	exit 1
 }
 
-file
-
-# No options
-echo files | sort -h -r | awk 'NR == 2 { printf($2) }'
+# No options, biggest size
+find $dir -type f | sort -n | head -n 1
 
 # Options
-if [ $arg1 = '-s' ]; then
-    echo files | sort -h | awk 'NR == 2 { printf($2) }'
+if [ "$arg1" == "-s" ]; then
+    find $dir -type f | sort -nr | head -n 1
 
-elif [ $arg1 = '-r' ]; then
+elif [ "$arg1" == "-r" ]; then
+	find $dir -maxdepth 1 -type f | sort -n | head -n 1
+
+elif [ "$arg1" == "-r" ] && [ "$arg2" == "-s" ]; then
+	find $dir -maxdepth 1 -type f | sort -nr | head -n 1
+
+fi
