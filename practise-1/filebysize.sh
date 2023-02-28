95150db28b9289f868ccc67a9fc41703cda31a43
Ioana Carmen Pasca <ic.pasca.2021@alumnos.urjc.es>
2023-02-28 11:12:32 +0100

    comments and functioning
    Signed-off-by: Ioana Carmen Pasca <ic.pasca.2021@alumnos.urjc.es>

--- practise-1/filebysize.sh
+++ practise-1/filebysize.sh
@@ -4,33 +4,35 @@
 #    File: filebysize.sh
 #    Author: Ioana Carmen Pasca
 #    Date: 25/02/2023
-#    Goal: Prints the file with the biggest size 
+#    Goal: Prints the file with the biggest size or smallest
 # -------------------------------------------------------------------
 
 # Constants
 arg1="$1"
 arg2="$2"
-dir=$(pwd)
 
 # Variables
-files=$(ls -l | awk '{ size=$5 ; name=$9; printf(size/1024 " " name "\n") }')
+dir=$(pwd)
 
 usage() {  # Error of usage
 	echo "usage: ./filebysize [OPTION]..." 1>&2
 	exit 1
 }
 
-# No options, biggest size
+# No options, biggest size file
 find $dir -type f | sort -n | head -n 1
 
 # Options
 if [ "$arg1" == "-s" ]; then
+	# Smallest size file
     find $dir -type f | sort -nr | head -n 1
 
 elif [ "$arg1" == "-r" ]; then
+	# Biggest size file, recursive search
 	find $dir -maxdepth 1 -type f | sort -n | head -n 1
 
 elif [ "$arg1" == "-r" ] && [ "$arg2" == "-s" ]; then
+	# Smallest size file, recursive search
 	find $dir -maxdepth 1 -type f | sort -nr | head -n 1
 
 fi
