0e361810d567e6422e808bec3207c7e6c4e0be8b
Ioana Carmen Pasca <ic.pasca.2021@alumnos.urjc.es>
2023-02-28 12:15:01 +0100

    beginning
    Signed-off-by: Ioana Carmen Pasca <ic.pasca.2021@alumnos.urjc.es>

--- practise-1/processes.sh
+++ practise-1/processes.sh
@@ -1,1 +1,16 @@
 #!/bin/sh
+
+# -----------------------------------------------------------------
+#    File: processes.sh
+#    Author: Ioana Carmen Pasca
+#    Date: 28/02/2023
+#    Goal: 
+# -------------------------------------------------------------------
+
+
+usage() {  # Error of usage
+	echo "usage: ./filebysize [OPTION]..." 1>&2
+	exit 1
+}
+
+case 
